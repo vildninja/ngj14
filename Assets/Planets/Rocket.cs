e60e5b325ec3e8362e5a17ab781c86e085c85b5b
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2014-02-15 16:53:36 +0100

    Rockets

--- Assets/Planets/Rocket.cs
+++ Assets/Planets/Rocket.cs
@@ -1,17 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Rocket : MonoBehaviour {
 
     public SpacePlayer target;
 
+
+    public List<Transform> avoid;
+    Vector3 direction = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
-	
+        avoid = new List<Transform>();
+        foreach (var sp in FindObjectsOfType<SpacePlayer>())
+            if (sp != target)
+                avoid.Add(sp.transform);
+        foreach (var p in FindObjectsOfType<PlanetController>())
+            avoid.Add(p.transform);
 	}
-	
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + direction);
+    }
+
 	// Update is called once per frame
 	void FixedUpdate () {
-	    
+        direction = (target.transform.position - transform.position).normalized * 4;
+        foreach (var t in avoid)
+            direction += (transform.position - t.transform.position).normalized * (5 / (t.transform.position - transform.position).magnitude);
+
+        if (rigidbody.velocity.magnitude > 3)
+        {
+            direction -= rigidbody.velocity;
+        }
+
+        rigidbody.AddForce(direction);
+
+        transform.up = rigidbody.velocity.normalized;
 	}
 }
--- Assets/Player/MovementScript.cs
+++ Assets/Player/MovementScript.cs
@@ -6,44 +6,51 @@ public class MovementScript : MonoBehaviour {
 	public float forceMultiplierForGyro = 10f;
 	public AnimationCurve curve;
 
+    private SpacePlayer sp;
+
+    void Awake()
+    {
+        sp = GetComponent<SpacePlayer>();
+    }
+
 	void FixedUpdate(){
-		//keyboard control
-		float vertical = Input.GetAxis ("Vertical") * force;
-		float horizontal = Input.GetAxis ("Horizontal") * force;
-		rigidbody.AddForce (new Vector3 (horizontal, vertical, 0));
+        if (networkView.owner == Network.player && sp.canMove)
+        {
+            //keyboard control
+            float vertical = Input.GetAxis("Vertical") * force;
+            float horizontal = Input.GetAxis("Horizontal") * force;
+            rigidbody.AddForce(new Vector3(horizontal, vertical, 0));
 
-		// we assume that device is held parallel to the ground
-		// and Home button is in the right hand
-		Vector3 dir = Vector3.zero;
-		
-		// remap device acceleration axis to game coordinates:
-		//  1) XY plane of the device is mapped onto XZ plane
-		//  2) rotated 90 degrees around Y axis
-		dir.x = Input.acceleration.x;
-		dir.y = Input.acceleration.y;
-		
-		//dir = Quaternion.Euler(0, 90, 0) * dir;
-		
-		// clamp acceleration vector to unit sphere
-		if (dir.sqrMagnitude > 1) {
-			dir.Normalize ();
-		}
+            // we assume that device is held parallel to the ground
+            // and Home button is in the right hand
+            Vector3 dir = Vector3.zero;
 
-		dir.x = Mathf.Sign(dir.x) * curve.Evaluate (dir.x);
-		dir.z = Mathf.Sign(dir.z) * curve.Evaluate (dir.z);
-		
-		// Make it move 10 meters per second instead of 10 meters per frame...
-		dir *= Time.deltaTime;
-		
-		// Move object
-		rigidbody.AddForce (dir * force * forceMultiplierForGyro);
+            // remap device acceleration axis to game coordinates:
+            //  1) XY plane of the device is mapped onto XZ plane
+            //  2) rotated 90 degrees around Y axis
+            dir.x = Input.acceleration.x;
+            dir.y = Input.acceleration.y;
+
+            //dir = Quaternion.Euler(0, 90, 0) * dir;
+
+            // clamp acceleration vector to unit sphere
+            if (dir.sqrMagnitude > 1)
+            {
+                dir.Normalize();
+            }
+
+            dir.x = Mathf.Sign(dir.x) * curve.Evaluate(dir.x);
+            dir.z = Mathf.Sign(dir.z) * curve.Evaluate(dir.z);
+
+            // Make it move 10 meters per second instead of 10 meters per frame...
+            dir *= Time.deltaTime;
+
+            // Move object
+            rigidbody.AddForce(dir * force * forceMultiplierForGyro);
+        }
 	}
 
 	void OnGUI(){
 		GUI.Label (new Rect (10, 10, 100, 100), Input.acceleration.x.ToString ()+ "     " + Input.acceleration.y.ToString());
-	}
-
-	void Update () {
-
 	}
 }
