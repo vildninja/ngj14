fff51eba1478790c3edf093e12aa883ef15bbeaf
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2014-02-16 14:30:16 +0100

    back to menu

---
+++ Assets/Menu/BackToMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToMenu : UIButton {
+
+    protected override void Up()
+    {
+        if (Network.isClient || Network.isServer)
+            Network.Disconnect();
+        Application.LoadLevel("Menu");
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        base.Update();
+        bool show = !FindObjectOfType<GameServer>().startGame;
+        renderer.enabled = show;
+        collider.enabled = show;
+	}
+}
