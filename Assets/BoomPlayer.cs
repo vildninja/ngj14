c857a920d87448120cb282ad1f205b2a804bcf7a
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2014-02-16 11:50:06 +0100

    more merge

--- Assets/BoomPlayer.cs
+++ Assets/BoomPlayer.cs
@@ -5,15 +5,7 @@ public class BoomPlayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
-<<<<<<< HEAD
 		Destroy (gameObject,5);
-=======
-		Destroy (transform,5);
->>>>>>> rollback
-=======
-		Destroy (transform,5);
->>>>>>> master
 	}
 	
 	// Update is called once per frame
