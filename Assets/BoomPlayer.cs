af6ff064fc9262b5d9960deaf6689184a752cd97
ZeppelinCaptain <victor.breum@gmail.com>
2014-02-16 10:42:07 +0100

    Particles!

---
+++ Assets/BoomPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoomPlayer : MonoBehaviour {
+
+	public float screenShakeAmount;
+	public float screenShakeTime;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (transform,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
